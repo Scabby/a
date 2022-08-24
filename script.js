8ade301be15da391b79885910998ca072d53476b
Scabby <61923737+Scabby@users.noreply.github.com>
2022-08-24 16:42:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+function start() {
+    if(pixel >= canvas.width * canvas.height) { return }
+    
+    let x = pixel % canvas.width
+    let y = Math.floor(pixel / canvas.width)
+    
+    canvas.fillRect(x, y, 1, 1)
+    
+    pixel++
+    requestAnimationFrame(start)
+}
+
 onready = () => {
     canvas  = document.getElementsByTagName("canvas")[0]
     ctx     = canvas.getContext("2d")
+    pixel   = 0
+    
+    ctx.fillStyle = "#000000"
+    start()
 }
