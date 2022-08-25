60c6977203c5a09637cb524404a384b4b51c9106
Scabby <61923737+Scabby@users.noreply.github.com>
2022-08-25 16:29:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
 function draw() {
-    refresh()
-    
-    /*
-    for(let p = 0; p < canvas.width * canvas.height; p++) {
-        let x = p % canvas.width
-        let y = Math.floor(p / canvas.width)
-        
-        if(Math.random() > 0.5) { ctx.fillStyle = "#000000" }
-        else                    { ctx.fillStyle = "#ffffff" }
-        
-        ctx.fillRect(x, y, 1, 1)
-    }
-    */
-    
-    ctx.fillStyle = "#ffffff"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    
-    requestAnimationFrame(draw)
+    clear()
+
+    //requestAnimationFrame(draw)
 }
 
-function set(x, y) {
-    ctx.fillRect(x, y, 1, 1)
+function resize() {
+    canvas.width    = window.innerWidth
+    canvas.height   = window.innerHeight
+    gl.viewport(0, 0, canvas.width, canvas.height)
 }
 
-function refresh() {
-    ctx.canvas.width    = window.innerWidth
-    ctx.canvas.height   = window.innerHeight
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+function clear() {
+    gl.clear(1, 0, 1, 1)
 }
+
+onresize = resize
 
 onload = () => {
     canvas  = document.getElementsByTagName("canvas")[0]
-    ctx     = canvas.getContext("2d")
-    pixel   = 0
+    gl      = canvas.getContext("webgl")
 
     requestAnimationFrame(draw)
 }
