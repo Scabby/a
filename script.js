9229844ba1a52b64492b9cb129251589e1ecdd4a
Scabby <61923737+Scabby@users.noreply.github.com>
2022-11-07 00:27:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function draw() {
-    clear()
-
-    //requestAnimationFrame(draw)
-}
-
-function resize() {
-    canvas.width    = window.innerWidth
-    canvas.height   = window.innerHeight
-    gl.viewport(0, 0, canvas.width, canvas.height)
-}
-
-function clear() {
-    gl.clearColor(1, 1, 1, 1)
-    gl.clear(gl.COLOR_BUFFER_BIT)
-}
-
-onresize = resize
-
 onload = () => {
-    canvas  = document.getElementsByTagName("canvas")[0]
-    gl      = canvas.getContext("webgl")
-
-    resize()
-    requestAnimationFrame(draw)
+    text  = document.getElementByTagName("p")
 }
